21f7563e56d6fed32b887435b6da03c79308204e
TienDung <dt0967236791@gmail.com>
2025-09-02 17:16:41 +0700

    PC-update

--- index.php
+++ index.php
@@ -91,7 +91,7 @@ if ($name !== null) $_SESSION['name'] = $name;
                     <img src="wind.png">
                     <div>
                         <p class="wind">15 km/h</p>
-                        <p>Wind Speed</p>
+                        <p>Tốc độ gió</p>
                     </div>
                 </div>
             </div>
